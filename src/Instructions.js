946f38773d7ce727eb1e5aed84917c40641f91ca
Thomas Barry <thomas@curmudgeon.local>
2020-05-04 13:34:59 -0400

    style updates

--- src/Instructions.js
+++ src/Instructions.js
@@ -1,37 +1,44 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
 import Box from "@material-ui/core/Box";
+import { makeStyles } from '@material-ui/core/styles';
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
-import "./styles.css";
 
-const Instructions = () => (
-  <Box px={2}>
-    <Box py={4}>
-      <Card className="instructions" raised={true}>
-        <CardContent>
-          <Typography align="left">
-            <p>
+const useStyles = makeStyles({
+  root: {
+    background: '#fff7e2',
+  },
+});
+
+const Instructions = () => {
+  const classes = useStyles()
+  return (
+    <Box px={2}>
+      <Box py={4}>
+        <Card className={classes.root} raised={true}>
+          <CardContent>
+            <Typography align="left">
               In this example, the SupportingText component renders its children
               when the input field has focus.
-            </p>
+            </Typography>
             <h4>Part One</h4>
-            <p>
-              Update the SupportingText component so its content becomes visible
-              when the input field receives focus, and remains visible after the
-              input field loses focus.
-            </p>
+            <Typography>
+                Update the SupportingText component so its content becomes visible
+                when the input field receives focus, and remains visible after the
+                input field loses focus.
+            </Typography>
             <h4>Part Two</h4>
-            <p>
+            <Typography>
               Update the SupportingText component so that its content fades into
               view.
-            </p>
-          </Typography>
-        </CardContent>
-      </Card>
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
     </Box>
-  </Box>
-);
+  )
+};
 
 export default Instructions;
